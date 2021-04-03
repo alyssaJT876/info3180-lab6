2abab145e173a244a720eb3d8feec23e44210c51
Alyssa Thompson <alyssaJT876@gmail.com>
2021-04-03 00:00:17 -0500

    Lab 6 Completed

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,11 +1,110 @@
 /* Add your Application JavaScript */
+const Home = {
+  name: 'Home',
+  template: `
+  <div class="home">
+    <img src="/static/images/logo.png" alt="VueJS Logo">
+    <h1>{{ welcome }}</h1>
+  </div>
+  `,
+  data() {
+    return {
+    welcome: 'Hello World! Welcome to VueJS'
+  }
+  }
+};
+
+
+const NewsList = {
+  name:'news-list',
+  template: `
+    <div class="news">
+      <h2>News</h2>
+
+      <div class="form-inline d-flex justify-content-center">
+
+        <div class="form-group mx-sm-3 mb-2">
+          <label class="sr-only" for="search">Search</label>
+          <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+          <button class="btn btn-primary mb-2"
+          @click="searchNews">Search</button>
+        </div>
+
+      </div>
+    <ul class="news__list">
+    <li v-for="article in articles" class="news__item">
+    <h5> {{ article.title }} </h5>
+    <img :src="article.urlToImage" class="news_pic">
+    <p> {{ article.description }} </p>
+    </li> 
+    </ul>
+    </div>
+  `,
+
+created(){
+let self = this;
+fetch('https://newsapi.org/v2/top-headlines?country=us',
+{
+  headers: {
+    'Authorization': 'Bearer 8edf113242a443888a7092c8ffe80311'
+    }
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      self.articles = data.articles;
+      });
+    },
+   data() {
+    return {
+      articles: [], 
+      searchTerm:''
+    }
+  }, 
+  methods: {
+    searchNews() {
+      let self = this;
+      fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en', {
+      headers: {
+        'Authorization': 'Bearer 8edf113242a443888a7092c8ffe80311'
+      }
+    })  
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      self.articles = data.articles;
+    });
+  }
+  } 
+};
+
+
 const app = Vue.createApp({
   data() {
     return {
       welcome: 'Hello World! Welcome to VueJS'
     }
-  }
+  },
+  components: {
+    'home': Home,
+    'news-list': NewsList
+    }
 });
+
+
+const router = VueRouter.createRouter({
+ history: VueRouter.createWebHistory(),
+ routes: [
+ { path: '/', component: Home },
+ { path: '/news', component: NewsList }
+ ]
+});
+
+
 
 app.component('app-header', {
   name: 'AppHeader',
@@ -20,10 +119,10 @@ app.component('app-header', {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <router-link to="/" class="nav-link">Home</router-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
+                  <router-link to="/news" class="nav-link">News</router-link>
                 </li>
               </ul>
             </div>
@@ -49,6 +148,9 @@ app.component('app-footer', {
           year: (new Date).getFullYear()
       }
   }
-})
+});
+
+
+app.use(router);
 
 app.mount('#app');
